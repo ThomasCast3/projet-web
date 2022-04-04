22869a3f66eb81c190e8fd1e91076fbdee96f284
EtienneRld <etienne.renauld@laposte.net>
2022-04-04 17:01:15 +0200

    Spring files

---
+++ src/test/java/coding/finalwebprojet/restapi/RestApiApplicationTests.java
@@ -0,0 +1,13 @@
+package coding.finalwebprojet.restapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
