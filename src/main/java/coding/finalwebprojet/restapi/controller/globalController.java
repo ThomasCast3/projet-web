2865eddf8ffb844e0c3470f9405e818cfc86506d
EtienneRld <etienne.renauld@laposte.net>
2022-04-05 12:11:24 +0200

    api global mappings, example mapping 'trainAttendance'

---
+++ src/main/java/coding/finalwebprojet/restapi/controller/globalController.java
@@ -0,0 +1,17 @@
+package coding.finalwebprojet.restapi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class globalController {
+    @GetMapping(value = "/")
+    public String index() {
+        return "<h1>Page d'accueil de l'api</h1>";
+    }
+
+    @GetMapping(value = "*")
+    public String error() {
+        return "<h1>ERROR 404 NOT FOUND</h1>";
+    }
+}
