fa70d366d1a6ef0be2b843bced2bc1a28193aca1
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2022-04-11 15:28:08 +0200

    Le dashboard
    Permet d'afficher les utilisateurs depuis l'API

---
+++ vuejs3-projetweb/src/components/ProfilInfo.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="Informations">
+    <h2>Vos informations</h2>
+    <div class="Informations-item">
+      <input
+        type="text"
+        class="Informations-Prenom"
+        placeholder="Prénom"
+        v-model="form.firstName"
+      />
+      {{ getUser() }}
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "infoPage",
+  components: {},
+  data() {
+    return {
+      form: {
+        firstName: "",
+        lastName: "",
+        mdp: "",
+        mail: "",
+      },
+    };
+  },
+  methods: {
+    getUser() {
+      const xmlHttpRequest = new XMLHttpRequest();
+      xmlHttpRequest.open("GET", "http://10.57.29.14:8080/get-users", true);
+      xmlHttpRequest.send();
+      xmlHttpRequest.onload = () => {
+        console.log(xmlHttpRequest.responseText);
+        document.querySelector(".Informations").innerHTML +=
+          xmlHttpRequest.responseText;
+      };
+    },
+    // getUserName(){
+    //     const xmlHttpRequest = new XMLHttpRequest();
+    //     xmlHttpRequest.open("GET", "http://10.57.29.14:8080/get-users", true);
+    //     xmlHttpRequest.send();
+    //     xmlHttpRequest.onload = () => {
+
+    //     let email = xmlHttpRequest.responseText;
+    //     for (let user in email){
+    //         console.log(user.email);
+    //     }
+    //   }
+    //   return user.email ;
+    // }
+  },
+  computed: {},
+};
+</script>
+
+<style scoped>
+.Informations {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  align-content: center;
+  margin: 1em;
+}
+</style>
---
+++ vuejs3-projetweb/src/views/Profil.vue
@@ -0,0 +1,119 @@
+<template>
+  <div class="container">
+    <div v-show="isConnected" class="Connecte">
+      <div class="dashboard-header">
+        <div class="dashboard-header-title">
+          <h1>Dashboard</h1>
+        </div>
+      </div>
+      <div class="dashboard">
+        <div class="dashboard-navbar">
+          <div class="dashboard-navbar-title">
+            <h2>Mon Dashboard</h2>
+          </div>
+          <div class="dashboard-navbar-infos">
+            <div class="dashboard-navbar-infos-item">
+              <div class="dashboard-navbar-infos-item-title">
+                <h3>Informations</h3>
+              </div>
+            </div>
+            <div class="dashboard-navbar-infos-item">
+              <div class="dashboard-navbar-infos-item-title">
+                <h3>Notifications</h3>
+              </div>
+            </div>
+            <div class="dashboard-navbar-infos-item">
+              <div class="dashboard-navbar-infos-item-title">
+                <h3>Mes trajets</h3>
+              </div>
+            </div>
+            <div class="dashboard-navbar-infos-item">
+              <div class="dashboard-navbar-delete">
+                <h3>Deconnexion</h3>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div class="informations">
+          <Info />
+        </div>
+      </div>
+    </div>
+    <div v-show="!isConnected" class="pasConnecte">
+      <strong>PAS CONNECTE</strong>
+    </div>
+  </div>
+</template>
+
+
+
+
+<script>
+import Info from "/src/components/ProfilInfo.vue";
+export default {
+  name: "profilePage",
+  components: {
+    Info,
+  },
+  data() {
+    return {
+      isConnected: true,
+    };
+  },
+  methods: {
+  },
+  computed: {},
+};
+</script>
+
+
+
+
+<style scoped>
+.dashboard {
+  display: flex;
+  flex-direction: row;
+}
+.dashboard-header-title {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  align-content: center;
+}
+.dashboard-navbar {
+  background-color: #eeeeee;
+  text-align: left;
+  padding: 1em;
+  border-radius: 1em;
+  box-shadow: 0px 0px 5px 0px rgba(0, 0, 0, 0.75);
+}
+.dashboard-navbar-infos {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  align-content: center;
+}
+.dashboard-content {
+  background-color: #cbcbcb;
+  text-align: left;
+}
+
+.dashboard-navbar-delete:hover {
+  background-color: rgb(225, 65, 65);
+  border: none;
+  border-radius: 1em;
+  padding-left: 1em;
+  padding-right: 1em;
+  cursor: pointer;
+}
+.dashboard-navbar-infos-item-title:hover {
+  background-color: #5c9ad4;
+  border: none;
+  border-radius: 1em;
+  padding-left: 1em;
+  padding-right: 1em;
+  cursor: pointer;
+}
+</style>
