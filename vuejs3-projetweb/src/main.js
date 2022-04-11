0b98991ec3fe6fd85c418b80821ef4437a2bd0c6
LouisYang95 <91289384+LouisYang95@users.noreply.github.com>
2022-04-11 15:28:34 +0200

    Les principaux js
    JS permettant d'etre en SPA

--- vuejs3-projetweb/src/main.js
+++ vuejs3-projetweb/src/main.js
@@ -1,9 +1,12 @@
-import { createApp } from 'vue'
-import router from './router';
-import App from './App.vue'
-import Inscription from './views/Inscription.vue'
+import { createApp } from "vue";
+import router from "./router";
+import App from "./App.vue";
+import Inscription from "./views/Inscription.vue";
+
 
 const app = createApp(App);
-app.Inscription = ("Inscriptions" , Inscription);
+
+
+app.Inscription = ("Inscriptions", Inscription);
 app.use(router);
-app.mount('#app');
+app.mount("#app");
--- vuejs3-projetweb/src/router/index.js
+++ vuejs3-projetweb/src/router/index.js
@@ -1,25 +1,52 @@
-import { createRouter, createWebHistory} from "vue-router";
+import { createRouter, createWebHistory } from "vue-router";
 import Inscription from "../views/Inscription.vue";
 import Home from "../views/Home.vue";
-
+import Connexion from "../views/Connexion.vue";
+import Contact from "../views/Contact.vue";
+import Profil from "../views/Profil.vue";
+import Users from "../views/Profil.vue";
+import Notifications from "../components/ProfilNotif.vue";
 const routes = [
-    {
-        path: "/",
-        name: "Home",
-        component: Home
-    },
-    {
-        path: "/Inscription",
-        name: "Inscription",
-        component: Inscription
-    }
-
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+  },
+  {
+    path: "/Inscription",
+    name: "Inscription",
+    component: Inscription,
+  },
+  {
+    path: "/Connexion",
+    name: "Connexion",
+    component: Connexion,
+  },
+  {
+    path: "/Contact",
+    name: "Contact",
+    component: Contact,
+  },
+  {
+    path: "/Profil",
+    name: "Profil",
+    component: Profil,
+  },
+  {
+    path: "/Profil/:id",
+    name: "Users",
+    component: Users,
+  },
+  {
+    path: "/Profil/Notifications",
+    name: "Notifications",
+    component: Notifications,
+  },
 ];
 
-
 const router = createRouter({
-    history: createWebHistory(),
-    routes
+  history: createWebHistory(),
+  routes,
 });
 
 export default router;
