3e01d61e2cae444719af3d31c49849bbd27e811c
ThomasCast3 <91255859+ThomasCast3@users.noreply.github.com>
2022-04-13 15:53:09 +0200

    ytfguhi

---
+++ viewjs-mainpage/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <Header />
+    <Accueil />
+    <Footer />
+  </div>
+</template>
+
+<script>
+import Header from "/src/assets/components/Header.vue";
+import Footer from "/src/assets/components/Footer.vue";
+import Accueil from "./assets/components/Accueil.vue";
+
+export default {
+  name: "App",
+  components: {
+    Header,
+    Footer,
+    Accueil,
+  },
+
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style>
+@import url("https://fonts.googleapis.com/css2?family=Poppins:wght@100&display=swap");
+body {
+  font-family: "Poppins", sans-serif;
+  margin: 0;
+}
+</style>
